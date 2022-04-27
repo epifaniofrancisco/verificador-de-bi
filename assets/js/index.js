edf7b7cb517962809ec43d8d04d7f6ac089cef03
Epifânio Francisco <epifaniofrancisco03@gmail.com>
2022-04-27 23:27:01 +0100

    nova verificação com js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,82 +1,57 @@
 var principal = document.querySelector(".principal");
 var btnVerificar = document.getElementById("verificar");
 var bilhetes = document.querySelector(".bilhetes");
-var fields = document.querySelectorAll("[required]");
 
-function validateField(field) {
-	function validarBI(bi) {
-		const regex = /\d{9}[A-Za-z]{2}\d{3}$/;
+const form = document.getElementById("form");
+const inputBI = document.getElementById("numero-bi-input");
 
-		return regex.test(bi);
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	checkInputs();
+});
+
+function checkInputs() {
+	const inputValue = inputBI.value.trim();
+
+	if (inputValue === "") {
+		// mostrar erro
+		// add classe
+		setErrorFor(inputBI, "Preencha esse campo");
+	} else if (!validarBI(inputValue)) {
+		setErrorFor(inputBI, "Digite um número de BI válido");
+	} else {
+		// adicionar a classe de sucesso
+		setSuccessFor(inputBI);
+		// ! Resolver o problema do button que tem que ser clicado duas vezes para funcionar
+		showBI()
 	}
-
-	function verifyErrors() {
-		let foundError = false;
-
-		for (let error in field.validity) {
-			if (field.validity[error] && !field.validity.valid) {
-				foundError = error;
-			}
-		}
-
-		return foundError;
-	}
-
-	function customMessage(typeError) {
-		const messages = {
-			text: {
-				valueMissing: "Digite um número de BI válido",
-			},
-		};
-
-		return messages[field.type][typeError];
-	}
-
-	function setCustomMessage(message) {
-		const spanError = field.parentNode.querySelector("span.error");
-
-		if (message) {
-			spanError.classList.add("active");
-			spanError.innerHTML = message;
-		} else {
-			spanError.classList.remove("active");
-			spanError.innerHTML = "";
-		}
-	}
-
-	return function () {
-		const error = verifyErrors();
-
-		if (error) {
-			const message = customMessage(error);
-			setCustomMessage(message);
-		} else {
-			if (!validarBI(field.value)) {
-				setCustomMessage("Digite um número de BI válido");
-			} else {
-				field.style.borderColor = "green";
-				setCustomMessage();
-				btnVerificar.addEventListener("click", () => {
-					principal.style.height = "403px";
-					bilhetes.style.display = "flex";
-				});
-			}
-		}
-	};
 }
 
-// Mudar o estilo e forma de validação
-function validar(event) {
-	const field = event.target;
-	const validation = validateField(field);
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const span = formControl.querySelector("span");
 
-	validation();
+	span.innerText = message;
+	span.classList.add("active");
+	formControl.className = "input-group error";
 }
-for (let field of fields) {
-	field.addEventListener("invalid", (event) => {
-		// Eliminar o bubble
-		event.preventDefault();
-		validar(event);
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+
+	formControl.className = "input-group success";
+}
+
+function validarBI(bi) {
+	const regex = /\d{9}[A-Za-z]{2}\d{3}$/;
+
+	return regex.test(bi);
+}
+
+function showBI() {
+	btnVerificar.addEventListener("click", () => {
+		principal.style.height = "403px";
+		bilhetes.style.display = "flex";
 	});
-	field.addEventListener("blur", validar);
 }
