841784836ab7b6783df1e6849bb92a13c632cdf4
Epifânio Francisco <epifaniofrancisco03@gmail.com>
2022-05-06 12:03:37 +0100

    fontes, junção de funções e ajustes nos estilos

--- assets/js/consumingAPI.js
+++ assets/js/consumingAPI.js
@@ -1,3 +1,4 @@
+
 // Bilhete parte frontal
 var primeiroNome = document.getElementById("primeiro-nome");
 var ultimoNome = document.getElementById("ultimo-nome");
@@ -19,20 +20,13 @@ var municipioNascimento = document.getElementById("municipio-nascimento");
 var provinciaNascimento = document.getElementById("provincia-nascimento");
 var dataNascimento = document.getElementById("data-nascimento");
 
+var sexo = document.getElementById("sexo");
 var estadoCivil = document.getElementById("estado-civil");
 
 var dataEmissao = document.getElementById("data-emissao");
 var dataValidade = document.getElementById("data-validade");
 
-var bi = document.getElementById("numero-bi-input").value;
-/* 
-! API ORIGINAL
-
-? const API = `https://api.gov.ao/consultarBI/v2/?bi=${bi}`;
-
-*/
-
-const API = "http://economia.awesomeapi.com.br/json/last/USD-BRL";
+const API = `https://api.gov.ao/consultarBI/v2/?bi=006677901LA049`;
 
 function getAPI(url) {
 	let request = new XMLHttpRequest();
@@ -46,28 +40,31 @@ var resposta = getAPI(API);
 var dados = JSON.parse(resposta);
 
 function postData() {
-    primeiroNome.innerHTML = "";
-    ultimoNome.innerHTML = "";
-    
-    primeiroNomePai.innerHTML = "";
-    ultimoNomePai.innerHTML = "";
-    
-    primeiroNomeMae.innerHTML = "";
-    ultimoNomeMae.innerHTML = "";
-    
-    numberoBI.innerHTML = "";
-    
-    enderecoResidencia.innerHTML = "";
-    bairroResidencia.innerHTML = "";
-    municipioResidencia.innerHTML = "";
+	primeiroNome.innerHTML = dados[0]["FIRST_NAME"];
+	ultimoNome.innerHTML = dados[0]["LAST_NAME"];
 
-    
-    municipioNascimento.innerHTML = "";
-    provinciaNascimento.innerHTML = "";
-    dataNascimento.innerHTML = "";
-    
-    estadoCivil.innerHTML = "";
-    
-    dataEmissao.innerHTML = "";
-    dataValidade.innerHTML = "";
+	primeiroNomePai.innerHTML = dados[0]["FATHER_FIRST_NAME"];
+	ultimoNomePai.innerHTML = dados[0]["FATHER_LAST_NAME"];
+
+	primeiroNomeMae.innerHTML = dados[0]["MOTHER_FIRST_NAME"];
+	ultimoNomeMae.innerHTML = dados[0]["MOTHER_LAST_NAME"];
+
+	numberoBI.innerHTML = dados[0]["ID_NUMBER"];
+
+	enderecoResidencia.innerHTML = dados[0]["RESIDENCE_ADDRESS"];
+	bairroResidencia.innerHTML = dados[0]["RESIDENCE_NEIGHBOR"];
+	municipioResidencia.innerHTML = dados[0]["RESIDENCE_MUNICIPALITY_NAME"];
+
+	municipioNascimento.innerHTML = dados[0]["BIRTH_MUNICIPALITY_NAME"];
+	provinciaNascimento.innerHTML = dados[0]["BIRTH_PROVINCE_NAME"];
+	dataNascimento.innerHTML = dados[0]["BIRTH_DATE"];
+
+	sexo.innerHTML = dados[0]["GENDER_NAME"];
+	estadoCivil.innerHTML = dados[0]["MARITAL_STATUS_NAME"];
+
+	dataEmissao.innerHTML = dados[0]["ISSUE_DATE"];
+	dataValidade.innerHTML = dados[0]["EXPIRY_DATE"];
 }
+
+postData();
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,36 @@
 var principal = document.querySelector(".principal");
 var btnVerificar = document.getElementById("verificar");
 var bilhetes = document.querySelector(".bilhetes");
-
 const form = document.getElementById("form");
 const inputBI = document.getElementById("numero-bi-input");
+
+// ! Dados do BI
+// Bilhete parte frontal
+var primeiroNome = document.getElementById("primeiro-nome");
+var ultimoNome = document.getElementById("ultimo-nome");
+
+var primeiroNomePai = document.getElementById("primeiro-nome-pai");
+var ultimoNomePai = document.getElementById("ultimo-nome-pai");
+
+var primeiroNomeMae = document.getElementById("primeiro-nome-mae");
+var ultimoNomeMae = document.getElementById("ultimo-nome-mae");
+
+var numberoBI = document.getElementById("numero-bi");
+
+// Bilhete parte traseira
+var enderecoResidencia = document.getElementById("endereco-residencia");
+var bairroResidencia = document.getElementById("bairro-residencia");
+var municipioResidencia = document.getElementById("municipio-residencia");
+
+var municipioNascimento = document.getElementById("municipio-nascimento");
+var provinciaNascimento = document.getElementById("provincia-nascimento");
+var dataNascimento = document.getElementById("data-nascimento");
+
+var sexo = document.getElementById("sexo");
+var estadoCivil = document.getElementById("estado-civil");
+
+var dataEmissao = document.getElementById("data-emissao");
+var dataValidade = document.getElementById("data-validade");
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
@@ -23,8 +50,8 @@ function checkInputs() {
 	} else {
 		// adicionar a classe de sucesso
 		setSuccessFor(inputBI);
-		// ! Resolver o problema do button que tem que ser clicado duas vezes para funcionar
-		showBI()
+		showData();
+		showBI();
 	}
 }
 
@@ -55,3 +82,57 @@ function showBI() {
 		bilhetes.style.display = "flex";
 	});
 }
+
+function showData() {
+	const oi = inputBI.value.trim();
+
+	const API = `https://api.gov.ao/consultarBI/v2/?bi=${oi}`;
+
+	
+
+	function getAPI(url) {
+		let request = new XMLHttpRequest();
+		request.open("GET", url, false);
+		request.send();
+
+		return request.responseText;
+	}
+
+	var resposta = getAPI(API);
+	var dados = JSON.parse(resposta);
+
+	function postData() {
+		primeiroNome.innerHTML = dados[0]["FIRST_NAME"];
+		ultimoNome.innerHTML = dados[0]["LAST_NAME"];
+
+		primeiroNomePai.innerHTML = dados[0]["FATHER_FIRST_NAME"];
+		ultimoNomePai.innerHTML = dados[0]["FATHER_LAST_NAME"];
+
+		primeiroNomeMae.innerHTML = dados[0]["MOTHER_FIRST_NAME"];
+		ultimoNomeMae.innerHTML = dados[0]["MOTHER_LAST_NAME"];
+
+		numberoBI.innerHTML = dados[0]["ID_NUMBER"];
+
+		enderecoResidencia.innerHTML = dados[0]["RESIDENCE_ADDRESS"];
+		bairroResidencia.innerHTML = dados[0]["RESIDENCE_NEIGHBOR"];
+		municipioResidencia.innerHTML = dados[0]["RESIDENCE_MUNICIPALITY_NAME"];
+
+		municipioNascimento.innerHTML = dados[0]["BIRTH_MUNICIPALITY_NAME"];
+		provinciaNascimento.innerHTML = dados[0]["BIRTH_PROVINCE_NAME"];
+		dataNascimento.innerHTML = dados[0]["BIRTH_DATE"];
+
+		sexo.innerHTML = dados[0]["GENDER_NAME"];
+		estadoCivil.innerHTML = dados[0]["MARITAL_STATUS_NAME"];
+
+		dataEmissao.innerHTML = dados[0]["ISSUE_DATE"];
+		dataValidade.innerHTML = dados[0]["EXPIRY_DATE"];
+	}
+
+	if (dados.length === 0) {
+		setErrorFor(inputBI, "Esse número do BI não existe! Tente novamente")
+	} else {
+		postData();
+	}
+}
+
+
